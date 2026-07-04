6d0c9eb7840aa8e67defb16ae237e98aa3f7f587
Krish Pawar <pawarkrish641@gmail.com>
2026-07-04 23:09:28 +0530

    Solution for Leetcode #6 Mergetwosortedarray

---
+++ Arrays/LC0006_MergetwosortedArray.java
@@ -0,0 +1,40 @@
+public class LC0006_MergetwosortedArray {
+    public static void main(String[] args) {
+        int[] a={1,2,5,7,9};
+        int[] b={3,4,6,8,10};
+        int[] c=new int[a.length+b.length];
+        merge(a,b,c);
+        for(int ele : c){
+            System.out.print(ele+" ");
+        }
+    }
+    public static void merge(int[] a, int[] b, int[] c){
+        int i=0,j=0,k=0;
+        while (i<a.length && j<b.length){
+            if(a[i]<b[j]){
+                c[k]=a[i];
+                i++;
+            }
+            else{
+                c[k]=b[j];
+                j++;
+            }
+            k++;
+        }
+        if(i==a.length){
+            while(j<b.length){
+                c[k]=b[j];
+                j++;
+                k++;
+            }
+        }
+        else{
+            while(i<a.length){
+                c[k]=a[i];
+                i++;
+                k++;
+            }
+        }
+    }
+    
+}
