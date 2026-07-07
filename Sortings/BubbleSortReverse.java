3bf15df1ca3cd50906f524e4db43a7a17b47b744
Krish Pawar <pawarkrish641@gmail.com>
2026-07-07 16:17:26 +0530

    New topic learned Sortings

---
+++ Sortings/BubbleSortReverse.java
@@ -0,0 +1,30 @@
+package Sortings;
+/*
+time Comeplexity=O(n)
+ */
+public class BubbleSortReverse {
+    public static void print(int[] arr){
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr={4,2,6,3,6,2,6,3,8,9,4};
+        int n=arr.length;
+        print(arr);
+        for(int i=n-1;i>0;i--){
+            int swaps=0;
+            for(int j=n-1;j>n-1-i;j--){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swaps++;
+                }
+            }
+           if(swaps==0) break;
+        }
+        print(arr);
+    }
+}
