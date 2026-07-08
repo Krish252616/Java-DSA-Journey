efff021c1bd1e0707974d08130ddeab0828810f3
Krish Pawar <pawarkrish641@gmail.com>
2026-07-08 13:12:21 +0530

    Solution for GeeksforGeeks #7 Movezerotoend

---
+++ Arrays/GFG0007_MovezerostoEnd.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class GFG0007_MovezerostoEnd {
+    public static void movezero(int[] arr){
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size : ");
+        int n=sc.nextInt();
+
+        int arr[]=new int[n];
+        System.out.println("Enter the elements : ");
+        for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+        }
+        sc.close();
+        movezero(arr);
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(arr[i] != 0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+        movezero(arr);
+    }
+    
+}
